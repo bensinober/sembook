8ff6d475159bbf155aee7d29696cf13b86a33518
benjab <benjab@benjab-kontor.(none)>
2012-02-01 09:16:58 +0100

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sembook::Application.config.secret_token = '8446175e389d24f5db79665aa24fb7a9cb67798126cc693f6f519dcc3b463fae946bcc28f559b55d17278fdd4e707ee36ee4cd051a357a3291ff9cb3b28d227b'
